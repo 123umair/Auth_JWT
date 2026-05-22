ef47b51d951ba66731ac3279ac186438a70bf29f
123Umair <khanuumair91@gmail.com>
2026-05-22 22:36:01 +0500

    Updated

--- Frontent/src/component/Page/Welcome.jsx
+++ Frontent/src/component/Page/Welcome.jsx
@@ -1,5 +1,25 @@
+import axios from 'axios'
+import { useNavigate } from 'react-router'
+
+
 
 const Welcome = () => {
+    const API = import.meta.env.VITE_API_URL
+    const navigate = useNavigate()
+    const handleLogout = async () => {
+        try {
+            const res = await axios.post(`${API}/logout`, {}, { withCredentials: true })
+            if (res) {
+                console.log(res.data.message, 'use is successfully logedout')
+                navigate('/')
+                return
+            }
+
+        }
+        catch (error) {
+            console.log("Error", error)
+        }
+    }
 
     return (
         <div className='min-h-screen flex flex-col items-center justify-center bg-gray-100'>
@@ -10,7 +30,13 @@ const Welcome = () => {
                 </h1>
                 <div className=''> Dear your loggedin</div>
             </div>
-
+            <button
+                type="submit"
+                className='w-full  text-blue-600 py-2 rounded-md transition'
+                onClick={() => { handleLogout() }}
+            >
+                Logout
+            </button>
         </div>
     )
 }
